49f017ef3a83e6209cedd0c88458c418c5ed461b
Kirsten <kcomandich@gmail.com>
2021-12-12 05:49:23 +1100

    Day 5 - Hydrothermal Venture: rough solution to part 2 only.

--- day_5/hydrothermal_venture.rb
+++ day_5/hydrothermal_venture.rb
@@ -14,20 +14,7 @@ class HydrothermalVenture
       max_x = x2 if x2 > max_x
       max_y = y1 if y1 > max_y
       max_y = y2 if y2 > max_y
-      if x1 == x2 # only consider horizontal and vertical lines for now
-        if y1 > y2
-          @lines << { :start => [x1, y2], :end => [x2, y1] }
-        else
-          @lines << { :start => [x1, y1], :end => [x2, y2] }
-        end
-      end
-      if y1 == y2 # only consider horizontal and vertical lines for now
-        if x1 > x2
-          @lines << { :start => [x2, y1], :end => [x1, y2] }
-        else
-          @lines << { :start => [x1, y1], :end => [x2, y2] }
-        end
-      end
+      @lines << { :start => [x1, y1], :end => [x2, y2] }
     end
     @diagram = Array.new(max_x + 1) { Array.new(max_y + 1) { 0 }}
     map_lines
@@ -41,16 +28,37 @@ class HydrothermalVenture
       y2 = line[:end][0]
       x2 = line[:end][1]
 #      puts "drawing line from #{y1},#{x1} to #{y2},#{x2}"
-      if x1 == x2
-        while( y1 <= y2 ) do
+      while( x1 != x2 || y1 != y2 ) do
+        if x1 == x2 &&  y1 < y2
           @diagram[x1][y1] += 1
           y1 += 1
-        end
-      else
-        while( x1 <= x2 ) do
+        elsif x1 == x2 && y1 > y2
+          @diagram[x1][y1] += 1
+          y1 -= 1
+        elsif y1 == y2 && x1 < x2
           @diagram[x1][y1] += 1
           x1 += 1
+        elsif y1 == y2 && x1 > x2
+          @diagram[x1][y1] += 1
+          x1 -= 1
+        elsif x1 < x2 && y1 < y2
+          @diagram[x1][y1] += 1
+          x1 += 1
+          y1 += 1
+        elsif x1 < x2 && y1 > y2
+          @diagram[x1][y1] += 1
+          x1 += 1
+          y1 -= 1
+        elsif x1 > x2 && y1 < y2
+          @diagram[x1][y1] += 1
+          x1 -= 1
+          y1 += 1
+        else
+          @diagram[x1][y1] += 1
+          x1 -= 1
+          y1 -= 1
         end
+        @diagram[x1][y1] += 1 if x1 == x2 && y1 == y2 # don't skip the end of the line
       end
     end
   end
