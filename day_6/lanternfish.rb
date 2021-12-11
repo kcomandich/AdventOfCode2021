ae3fae297a2043084aa5930115c94aaa004f2214
Kirsten <kcomandich@gmail.com>
2021-12-12 06:59:09 +1100

    Day 6 - Lantern Fish: better solution which solves part 1 and part 2 with very little memory. Rather than keeping an array with an element for each fish, keep an array representing one element for each possible reproduction day, so the maximum length of the array is 9, for the 0-8 days, storing the number of fish at each day. So the 0th element stores the number of fish that are at reproductive day 0.

--- day_6/lanternfish.rb
+++ day_6/lanternfish.rb
@@ -1,29 +1,40 @@
 require 'pry-byebug'
 
 class LanternFish
-  attr_accessor :fish
-
   def initialize
-    @fish = IO.read("input.txt").split(',').map(&:to_i)
+    input = IO.read("input.txt").split(',').map(&:to_i)
+    @fish = Array.new(9) { 0 }
+    input.each do |f|
+      @fish[f] += 1
+    end
   end
 
   def new_day
     current_fish = @fish.dup
     current_fish.each.with_index do |f,i|
-      if f == 0
-        @fish[i] = 6
-        @fish << 8
+      if i == 0
+        @fish[6] = f
+        @fish[8] = f
+      elsif i == 7
+        @fish[6] += f
       else
-        @fish[i] -= 1
+        @fish[i-1] = f
       end
     end
+  end
+
+  def number_of_fish
+    puts "Fish: "
+    pp @fish
+    @fish.sum
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   lf = LanternFish.new
-  80.times do |day|
+  num = 256
+  num.times do |day|
     lf.new_day
   end
-  puts "Final number of lantern fish: #{lf.fish.size}"
+  puts "Final number of lantern fish after #{num} days: #{lf.number_of_fish}"
 end
